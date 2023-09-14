facd1f7ac349395637d9ac6eac6e02c587459c15
naveend5050 <dnani304@gmail.com>
2023-09-15 01:37:48 +0530

    added abbrivation .js

--- function.js
+++ function.js
@@ -1,12 +1,6 @@
-<html>
-<head>
-    <style>functions in js</style>
+
+   
     let a=3;
     let b=7;
-    var z=a+b;
-    console.log(z);
-</head>
-<body>
-    
-</body>
-</html>
+    alert =(a+b);
+  
