f14f9740d3356fb115ebd74834e6fb0d2894201f
naveend5050 <dnani304@gmail.com>
2023-09-15 18:58:28 +0530

    added toUppercase operation

---
+++ assignment 1/question 4.js
@@ -0,0 +1,16 @@
+//4)Write a JavaScript function to capitalize the first letter of a string.
+//Test Data :
+//console.log(capitalize('js string exercises'));
+//"Js string exercises"
+
+
+// let name3='ramyatatiparti';
+// name2=name3.toUpperCase().charAt(0);
+// name4=name2+name3.slice(1);
+// console.log(name4);
+
+let name2='Dwarapudi naveen';
+name3=name2.toLowerCase().charAt(0);
+console.log(name3);
+name4=name3+name2.slice(1);
+console.log(name4);
