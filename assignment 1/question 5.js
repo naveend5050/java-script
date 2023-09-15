28dff336aae70bdc61e3324feb2c8becd3b4b62d
naveend5050 <dnani304@gmail.com>
2023-09-15 19:06:20 +0530

    added toLowercase letter

---
+++ assignment 1/question 5.js
@@ -0,0 +1,15 @@
+//5)Write a JavaScript function to uncapitalize the first character of a string.
+//Test Data :
+//console.log(Uncapitalize('Js string exercises'));
+//"js string exercises"
+
+// let namep='RAMYATHATIPARTI';
+// name2=namep.toLowerCase().charAt(0);
+// namee=name2+namep.slice(1);
+// console.log(namee);
+
+let run="DAWRAPUDI NAVEEN";
+stop=run.toLowerCase().charAt(0);
+console.log(stop);
+slow=stop+run.slice(1);
+console.log(slow);
