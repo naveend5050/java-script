34bfc4d5e343df46a4d98215a8bf807135aa8962
naveend5050 <dnani304@gmail.com>
2023-09-15 19:37:48 +0530

    added truncate_string length

---
+++ assignment 1/question 7.js
@@ -0,0 +1,18 @@
+//7)console.log(truncateText("This is a long sentence.", 10));
+// Output: "This is a..."
+//console.log(truncateText("Short text.", 15));
+// Output: "Short text."
+
+// let text="This is a long sentence.";
+// const truncate_string= text.slice(0,10);
+// console.log(truncate_string + '........');
+// let smalltext="ramya";
+// const truncate_string1=smalltext.slice(0,15);
+// console.log(truncate_string1);
+
+let text="iam making chapthi right now";
+const truncate_string=text.slice(0,10);
+console.log(truncate_string);
+let small="naveen";
+const truncate_string1=small.slice(0,15);
+console.log(truncate_string1);
