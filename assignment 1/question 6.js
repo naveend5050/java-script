832bd98a6bd4817c9e44baae88e6ced28a8446d0
naveend5050 <dnani304@gmail.com>
2023-09-15 19:18:12 +0530

    added trucate_string.........

---
+++ assignment 1/question 6.js
@@ -0,0 +1,14 @@
+//6)Write a function truncateText that takes a string and a maximum length as input and returns a
+//truncated version of the string if it exceeds the maximum length, adding "..." at the end.
+
+
+// const fname = 'ramyathatiparti';
+// const truncate_string= fname.slice(0,5);
+// const lname= truncate_string+ "....";
+// console.log(lname);
+
+const me="naveen nani";
+const truncate_string=me.slice(0,6);
+console.log(truncate_string);
+const you=truncate_string+".....";
+console.log(you);
