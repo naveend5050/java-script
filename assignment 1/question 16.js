83c9526626499b9827d1bd91c11b27c2941ca3cc
naveend5050 <dnani304@gmail.com>
2023-09-16 01:28:52 +0530

    select color

---
+++ assignment 1/question 16.js
@@ -0,0 +1,25 @@
+// 16)Color Selection
+// Write a function selectColor that takes a color choice ("red", "blue", "green") and whether the
+// color is available (true or false). If the color is available and the choice is "blue" or "green,"
+// return "Selected." If the choice is "red," return "Not available." For any other choice, return
+// "Invalid choice."
+
+
+function selectColor(x){
+
+    if (x== "red"|| x=="blue" || x=="green"){
+        return "true";
+    }
+ 
+   else if(x=="blue" || x=="green")
+   {
+    return "selected";
+   }
+   else if(x=="red"){
+    return "not available";    
+   }
+   return "invalid choice";
+
+ }
+ let x="orange";
+ console.log(selectColor(x));
