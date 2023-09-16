95166129415ec70b7fe8fed193f3039f2ede33f2
naveend5050 <dnani304@gmail.com>
2023-09-16 23:13:29 +0530

    Multiplication Table

---
+++ assignmet 2/question 4.js
@@ -0,0 +1,15 @@
+// **4. Print Multiplication Table:**
+// Write a program to print the multiplication table of a given number `n` using a `for` loop.
+
+// **Example:**
+// ```javascript
+// const n = 5;
+// for (let i = 1; i <= 10; i++) {
+//   console.log(`${n} x ${i} = ${n * i}`);
+// }
+
+const n=8;
+for (let i=1;i<=12;i++){
+    console.log(`${n} x ${i} = ${n*i}`);
+}
+
