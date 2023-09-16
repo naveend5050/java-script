c887273e342c5154832444f86ee3c72037712dcf
naveend5050 <dnani304@gmail.com>
2023-09-16 22:34:42 +0530

    Sum of Natural Numbers

---
+++ assignmet 2/question 1.js
@@ -0,0 +1,19 @@
+// **1. Sum of Natural Numbers:**
+// Write a program to calculate the sum of natural numbers from 1 to `n` using a `for` loop.
+
+// **Example:**
+// ```javascript
+// const n = 5;
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+// }
+// console.log(sum); // 15
+
+const n=5;
+let sum=0;
+for (let i=1;i<=n;i++){
+    sum +=i;
+}
+console.log(sum);
+
