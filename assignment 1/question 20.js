70d4f6650bc018d4460aff837d24038fa7a6a8d8
naveend5050 <dnani304@gmail.com>
2023-09-16 18:01:58 +0530

    shoppingList remove one element

---
+++ assignment 1/question 20.js
@@ -0,0 +1,20 @@
+// 20): Shopping List
+// Create an empty array called `shoppingList`. Using the appropriate methods, add the items
+// "eggs", "milk", "bread", and "vegetables" to the shopping list. Then, remove the "bread" from the
+// list.
+// Print the shopping list after each operation.
+
+
+let shoppingList= [];
+shoppingList[0]="eggs";
+shoppingList[1]="milk";
+shoppingList[2]="bread";
+shoppingList[3]="vegetables";
+shoppingList.slice(0,1);
+console.log(shoppingList);
+
+
+// shoppingList.push("eggs","milk","bread","vegetables");
+// console.log(shoppinglist);
+
+
