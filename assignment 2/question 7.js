a7de543515be8baed64c36433a8581d6db2bf4b7
naveend5050 <dnani304@gmail.com>
2023-10-23 15:20:20 +0530

    all the reduce method

---
+++ assignment 2/question 7.js
@@ -0,0 +1,14 @@
+// **7. Count Digits in a Number:**
+// Write a program to count the number of digits in a given number `n` using a `for` loop.
+// **Example:**
+// ```javascript
+// const n = 12345;
+// let count = 0;
+// let temp = n;
+// while (temp !== 0) {
+// count++;
+// temp = Math.floor(temp / 10);
+// }
+// console.log(count); // 5
+
+
